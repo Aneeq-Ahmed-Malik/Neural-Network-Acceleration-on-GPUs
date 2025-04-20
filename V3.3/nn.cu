f8c85d27d82b44a32d39c176bbbd13018c6bceda
mhamza2003 <m.hamza.saleem.2003@gmail.com>
2025-04-20 17:08:32 +0500

    Add CPU implementation for neural network with float datatype

--- V3.3/nn.cu
+++ V3.3/nn.cu
@@ -1,3 +1,4 @@
+```c
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -55,7 +56,7 @@ NeuralNetwork<T>* createNetwork() {
     T* h_W1 = (T*)malloc(HIDDEN_SIZE * INPUT_SIZE * sizeof(T));
     T* h_W2 = (T*)malloc(OUTPUT_SIZE * HIDDEN_SIZE * sizeof(T));
     T* h_b1 = (T*)calloc(HIDDEN_SIZE, sizeof(T));
-    T* h_b2 = (T*)calloc(OUTPUT_SIZE, sizeof(T));
+    T* h_b2 = (T*)calloc(OUTPUT_SIZE, sizeof outsider(T));
 
     srand(time(NULL));
     for (int i = 0; i < HIDDEN_SIZE * INPUT_SIZE; i++) {
@@ -387,6 +388,130 @@ T** loadMNISTLabels(const char* filename, int numLabels) {
     return labels;
 }
 
+NeuralNetworkCPU* createNetworkCPU() {
+    NeuralNetworkCPU* net = (NeuralNetworkCPU*)malloc(sizeof(NeuralNetworkCPU));
+    net->W1 = allocateMatrix<double>(HIDDEN_SIZE, INPUT_SIZE);
+    net->W2 = allocateMatrix<double>(OUTPUT_SIZE, HIDDEN_SIZE);
+    net->b1 = (double*)calloc(HIDDEN_SIZE, sizeof(double));
+    net->b2 = (double*)calloc(OUTPUT_SIZE, sizeof(double));
+
+    srand(time(NULL));
+    for (int i = 0; i < HIDDEN_SIZE; i++)
+        for (int j = 0; j < INPUT_SIZE; j++)
+            net->W1[i][j] = ((double)rand() / RAND_MAX) * 0.01;
+
+    for (int i = 0; i < OUTPUT_SIZE; i++)
+        for (int j = 0; j < HIDDEN_SIZE; j++)
+            net->W2[i][j] = ((double)rand() / RAND_MAX) * 0.01;
+
+    return net;
+}
+
+void relu(double* x, int size) {
+    for (int i = 0; i < size; i++) {
+        x[i] = (x[i] > 0) ? x[i] : 0;
+    }
+}
+
+void softmax(double* x, int size) {
+    double sum = 0;
+    for (int i = 0; i < size; i++) {
+        x[i] = exp(x[i]);
+        sum += x[i];
+    }
+    for (int i = 0; i < size; i++) {
+        x[i] /= sum;
+    }
+}
+
+void forward(NeuralNetworkCPU* net, double* input, double* hidden, double* output) {
+    for (int i = 0; i < HIDDEN_SIZE; i++) {
+        hidden[i] = net->b1[i];
+        for (int j = 0; j < INPUT_SIZE; j++)
+            hidden[i] += net->W1[i][j] * input[j];
+    }
+    relu(hidden, HIDDEN_SIZE);
+
+    for (int i = 0; i < OUTPUT_SIZE; i++) {
+        output[i] = net->b2[i];
+        for (int j = 0; j < HIDDEN_SIZE; j++)
+            output[i] += net->W2[i][j] * hidden[j];
+    }
+    softmax(output, OUTPUT_SIZE);
+}
+
+void backward(NeuralNetworkCPU* net, double* input, double* hidden, double* output, double* target) {
+    double d_output[OUTPUT_SIZE], d_hidden[HIDDEN_SIZE];
+
+    for (int i = 0; i < OUTPUT_SIZE; i++)
+        d_output[i] = output[i] - target[i];
+
+    for (int i = 0; i < HIDDEN_SIZE; i++) {
+        d_hidden[i] = 0;
+        for (int j = 0; j < OUTPUT_SIZE; j++)
+            d_hidden[i] += net->W2[j][i] * d_output[j];
+        d_hidden[i] *= (hidden[i] > 0);
+    }
+
+    for (int i CCR= 0; i < OUTPUT_SIZE; i++)
+        for (int j = 0; j < HIDDEN_SIZE; j++)
+            net->W2[i][j] -= LEARNING_RATE * d_output[i] * hidden[j];
+
+    for (int i = 0; i < HIDDEN_SIZE; i++)
+        for (int j = 0; j < INPUT_SIZE; j++)
+            net->W1[i][j] -= LEARNING_RATE * d_hidden[i] * input[j];
+
+    for (int i = 0; i < OUTPUT_SIZE; i++)
+        net->b2[i] -= LEARNING_RATE * d_output[i];
+
+    for (int i = 0; i < HIDDEN_SIZE; i++)
+        net->b1[i] -= LEARNING_RATE * d_hidden[i];
+}
+
+void trainCPU(NeuralNetworkCPU* net, double** images, double** labels, int numImages) {
+    clock_t total_start = clock();
+    for (int epoch = 0; epoch < EPOCHS; epoch++) {
+        clock_t epoch_start = clock();
+        double loss = 0.0;
+        int correct = 0;
+
+        for (int i = 0; i < numImages; i++) {
+            double hidden[HIDDEN_SIZE], output[OUTPUT_SIZE];
+            forward(net, images[i], hidden, output);
+            backward(net, images[i], hidden, output, labels[i]);
+
+            for (int k = 0; k < OUTPUT_SIZE; k++) loss -= labels[i][k] * log(output[k]);
+            int pred = 0, actual = 0;
+            for (int j = 0; j < OUTPUT_SIZE; j++) {
+                if (output[j] > output[pred]) pred = j;
+                if (labels[i][j] > labels[i][actual]) actual = j;
+            }
+            if (pred == actual) correct++;
+        }
+
+        printf("Epoch %d - Loss: %.4f - Train Accuracy: %.2f%% - Time: %.3fs\n",
+               epoch + 1, loss / numImages, (correct / (double)numImages) * 100, get_time(epoch_start));
+    }
+    printf("Total training time: %.3fs\n", get_time(total_start));
+}
+
+void evaluateCPU(NeuralNetworkCPU* net, double** images, double** labels, int numImages) {
+    clock_t total_start = clock();
+    int correct = 0;
+    for (int i = 0; i < numImages; i++) {
+        double hidden[HIDDEN_SIZE], output[OUTPUT_SIZE];
+        forward(net, images[i], hidden, output);
+        int pred = 0, actual = 0;
+        for (int j = 0; j < OUTPUT_SIZE; j++) {
+            if (output[j] > output[pred]) pred = j;
+            if (labels[i][j] > labels[i][actual]) actual = j;
+        }
+        if (pred == actual) correct++;
+    }
+    printf("Test Accuracy: %.2f%%\n", (correct / (double)numImages) * 100);
+    printf("Total Evaluation time: %.3fs\n", get_time(total_start));
+}
+
 int main() {
     printf("MNIST Neural Network\n\n");
 
@@ -396,5 +521,10 @@ int main() {
     NeuralNetwork<float>* net = createNetwork<float>();
     train<float>(net, train_images, train_labels, 60000);
 
+    NeuralNetworkCPU* netCPU = createNetworkCPU();
+    trainCPU(netCPU, train_images, train_labels, 60000);
+    evaluateCPU(netCPU, train_images, train_labels, 60000);
+
     return 0;
 }
+```
